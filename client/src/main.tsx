ffbf48de879bfd921aedf26489bcaea24e656739
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-04-13 19:47:49 +0200

    ok

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,15 +5,16 @@ import "./assets/index.css"
 import { DateProvider } from './hooks/DateContextHook.tsx'
 import { LeagueProvider } from "./hooks/LeaguesContextHook";
 import { GamesProvider } from './hooks/GamesContextHook.tsx'
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GamesProvider>
-      <LeagueProvider>
-        <DateProvider>
+    <DateProvider>
+      <GamesProvider>
+        <LeagueProvider>
           <App />
-        </DateProvider>
-      </LeagueProvider>
-    </GamesProvider>
-  </React.StrictMode >,
+          <Analytics />
+        </LeagueProvider>
+      </GamesProvider>
+    </DateProvider>
+  </React.StrictMode>
 )
